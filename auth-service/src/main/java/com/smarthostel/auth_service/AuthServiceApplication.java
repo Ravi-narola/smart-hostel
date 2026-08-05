b944307b3aa84d65e95bf827c8bb507f45e97bae
Ravi-narola <ravinarola3334@gmail.com>
2026-08-05 21:40:39 +0530

    feat: setup auth service

---
+++ auth-service/src/main/java/com/smarthostel/auth_service/AuthServiceApplication.java
@@ -0,0 +1,20 @@
+package com.smarthostel.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
