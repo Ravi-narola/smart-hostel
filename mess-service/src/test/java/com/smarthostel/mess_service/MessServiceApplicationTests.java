ade1ca2cc1b39d21ab7abb0a85382487954aa290
PushpakVariya <pushpakvariya2002@gmail.com>
2026-08-10 16:54:08 +0530

    feat: setup mess service

---
+++ mess-service/src/test/java/com/smarthostel/mess_service/MessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthostel.mess_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
