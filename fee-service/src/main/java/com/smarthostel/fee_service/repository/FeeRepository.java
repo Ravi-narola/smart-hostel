469859ef3acf35b582395918304b7ab5137f03ad
Ravi-narola <ravinarola3334@gmail.com>
2026-08-11 11:57:58 +0530

    feat: add fee repository

---
+++ fee-service/src/main/java/com/smarthostel/fee_service/repository/FeeRepository.java
@@ -0,0 +1,22 @@
+package com.smarthostel.fee_service.repository;
+
+import com.smarthostel.fee_service.entity.Fee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FeeRepository extends JpaRepository<Fee, Long> {
+
+    List<Fee> findByStudentId(Long studentId);
+
+    List<Fee> findByStudentIdAndStatus(
+            Long studentId,
+            String status
+    );
+
+    List<Fee> findByStatus(String status);
+
+    List<Fee> findByFeeType(String feeType);
+}
